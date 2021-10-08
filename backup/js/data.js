7dac29fe97fd872310d70085711c17185a46d070
quimeranoxia <dreiruivo@gmail.com>
2021-10-07 23:24:01 -0300

    a

---
+++ backup/js/data.js
@@ -0,0 +1,271 @@
+
+	// !!!!!!!!!!!!!!!!
+	// ABOUT
+	// !!!!!!!!!!!!!!!!
+
+		// Navigation
+			var navi = [];
+
+						// Language
+
+							// when it's in english
+								navi[0] = "Em Português";
+							// when it's in portuguese
+								navi[1] = "In English";
+
+								// Language
+
+
+						// Actual navi
+
+							// eng
+								navi[2] = "About";
+							// ptbr
+								navi[3] = "Sobre";
+
+							// eng
+								navi[4] = "Experience";
+							// ptbr
+								navi[5] = "Experiência";
+
+							// eng
+								navi[6] = "Education";
+							// ptbr
+								navi[7] = "Formação";
+
+							// eng
+								navi[8] = "Projects";
+							// ptbr
+								navi[9] = "Projetos";
+
+							// eng
+								navi[10] = "Skills";
+							// ptbr
+								navi[11] = "Habilidades";
+
+							// eng
+								navi[12] = "Contact";
+							// ptbr
+								navi[13] = "Contato";
+
+								// eng
+									navi[14] = "Close";
+								// ptbr
+									navi[15] = "Fechar";
+
+
+
+		// NAME etc
+
+			var myname = [];
+							// eng
+								myname[0] = "puta que pariu";
+							// ptbr
+								myname[1] = "Gabriella Ruivo";
+
+		// about
+
+			var about = [];
+
+						// actual about
+
+							// english
+								about[0] = "Hey there, <b style = 'color: #2781B3;'>Gabriella Ruivo</b> here!<br><br>I'm a Brazilian Digital Media Design student at university PUC-Rio, currently in my 4th semester and an intern at <a href = 'http://www.icad.puc-rio.br/'>ICAD Games</a>. <br><br>Drowned in <b>curiosity</b>, I consider myself an interdisciplinary kinda person. My heart beats both for art and technology, but though i seek to specialize in illustration and animation, I make a point to not limit my fascination to any field, and like to see them connecting. The world is too big and interesting!<br><br>This means I also have a <b>wide range of tastes</b>: my interests go from wholesome children's stories to the occasional psychological horror. I also have a thing for glitch art and old-school tech aesthetics, as it shows in this 90s/synthwave-esque page I've coded.<br><br>I strive to show my passion through my work, giving it my all, and am very adaptable. If you have any nice ideas, let's talk!";
+							// portuguese
+								about[1] = "Hey, <b style = 'color: #2781B3;'>Gabriella Ruivo</b> aqui!<br><br>Sou estudante de Design de Mídia Digital na PUC-Rio, atualmente no meu quarto semestre e estagiando no <a href = 'http://www.icad.puc-rio.br/'>ICAD Games</a>.<br><br>Afogada em <b>curiosidade</b>, eu me considero uma pessoa interdisciplinar. Meu coração bate para arte e tecnologia, e embora eu procure me especializar em ilustração e animação, não limito minha fascinação para nenhum campo, e gosto de vê-los se conectando. O mundo é grande e interessante demais!<br><br>Isso quer dizer que eu tenho uma <b>ampla gama de gostos</b>: meus interesses vão de histórias infantis até o ocasional horror psicológico. Também tenho algo por arte glitch e estéticas de tech retrô, como é possível ver nessa presente página anos 90/meio synthwave que codei.<br><br>Eu procuro mostrar minha paixão pelas coisas através do meu trabalho, dando meu melhor, e sou muito adaptável. Se você tiver alguma ideia, vamos conversar!";
+
+	// !!!!!!!!!!!!!!!!
+	// AUTOMATIC TRANSLATIONS ENG & PTBR
+	// !!!!!!!!!!!!!!!!
+
+		// INDEX
+
+
+				// top
+
+					// english
+//						index[2] = "languages.exe";
+					// portuguese
+//						index[3] = "idioma.exe";
+
+				// botão
+
+					// english
+//						index[4] = "About & Contact";
+					// portuguese
+//						index[5] = "Sobre & Contato";
+
+
+				// links
+
+					// english
+//						index[6] = "Illustrations";
+//						index[8] = "Animation [soon!]";
+					// portuguese
+//						index[7] = "Ilustracoes";
+//						index[9] = "Animacao [em breve!]";
+
+		// ILLUSTRATION
+
+
+		var illust = [];
+
+				// top
+
+					// english
+						illust[0] = "illustration.zip";
+					// portuguese
+						illust[1] = "ilustracao.zip";
+
+				// botões
+
+					// english
+						illust[2] = "Back";
+					// portuguese
+						illust[3] = "Voltar";
+
+		var design = [];
+
+				// botões
+
+					// english
+						design[0] = "Back";
+					// portuguese
+						design[1] = "Voltar";
+
+		// ABOUT
+
+				// top
+
+					// english
+						about[2] = "about.txt";
+					// portuguese
+						about[3] = "sobre.txt";
+
+				// botões
+
+					// english
+						about[4] = "Back";
+					// portuguese
+						about[5] = "Voltar";
+
+	// !!!!!!!!!!!!!!!!
+	// BIBLIOTECA
+	// !!!!!!!!!!!!!!!!
+
+		// DESCRIÇÕES
+
+
+				// english
+
+						var desc_shu01						= "2020";
+						var desc_mika01						= "2020";
+						var desc_rinne01					= "2020";
+						var desc_asch01						= "2020";
+						var desc_noi01						= "2020";
+						var desc_bubbles					= "2019";
+						var desc_narancia01					= "2019";
+						var desc_bede01						= "2019";
+						var desc_mother3_photography		= "2019";
+						var desc_projeto01					= "2019";
+						var desc_giorno01					= "2019";
+						var desc_holymother					= "2018";
+
+				// portuguese
+
+
+		// THUMBNAILS
+
+				// original
+
+					var thumb_shu01						= "url('thumbs/thumb_shu01.jpg')";
+					var thumb_mika01					= "url('thumbs/thumb_mika01.jpg')";
+					var thumb_rinne01					= "url('thumbs/thumb_rinne01.jpg')";
+					var thumb_asch01					= "url('thumbs/thumb_asch01.jpg')";
+					var thumb_noi01						= "url('thumbs/thumb_noi01.jpg')";
+					var thumb_bubbles					= "url('thumbs/thumb_bubbles.jpg')";
+					var thumb_narancia01				= "url('thumbs/thumb_narancia01.jpg')";
+					var thumb_bede01					= "url('thumbs/thumb_bede01.jpg')";
+					var thumb_mother3_photography		= "url('thumbs/thumb_mother3_photography.jpg')";
+					var thumb_projeto01					= "url('thumbs/thumb_projeto01.jpg')";
+					var thumb_giorno01					= "url('thumbs/thumb_giorno01.jpg')";
+					var thumb_holymother				= "url('thumbs/thumb_holymother.jpg')";
+
+
+
+
+		// IMAGENS AMPLIADAS
+
+				// original
+
+					var full_shu01						= "illusts/shu01.jpg";
+					var full_mika01						= "illusts/mika01.jpg";
+					var full_rinne01					= "illusts/rinne01.jpg";
+					var full_asch01						= "illusts/asch01.jpg";
+					var full_noi01						= "illusts/noi01.jpg";
+					var full_bubbles					= "illusts/bubbles.jpg";
+					var full_narancia01					= "illusts/narancia01.jpg";
+					var full_bede01						= "illusts/bede01.jpg";
+					var full_mother3_photography		= "illusts/mother3_photography.jpg";
+					var full_projeto01					= "illusts/projeto01.jpg";
+					var full_giorno01					= "illusts/giorno01.jpg";
+					var full_holymother					= "illusts/holymother.jpg";
+
+
+
+
+	// !!!!!!!!!!!!!!!!
+	// PAGE: ILLUST - ORDEM DAS ILUSTRAÇÕES
+	// !!!!!!!!!!!!!!!!
+
+		var max_orig =	12;
+
+
+		// thumbnails
+
+			// original
+
+				var orig_thumb = [];
+
+				orig_thumb[0] = thumb_shu01;
+				orig_thumb[1] = thumb_mika01;
+				orig_thumb[2] = thumb_rinne01;
+				orig_thumb[3] = thumb_asch01;
+				orig_thumb[4] = thumb_noi01;
+				orig_thumb[5] = thumb_bubbles;
+				orig_thumb[6] = thumb_narancia01;
+				orig_thumb[7] = thumb_bede01;
+				orig_thumb[8] = thumb_mother3_photography;
+				orig_thumb[9] = thumb_projeto01;
+				orig_thumb[10] = thumb_giorno01;
+				orig_thumb[11] = thumb_holymother;
+
+		// imagens ampliadas
+
+			// original
+
+				var orig_full = [];
+
+				orig_full[0] = full_shu01;
+				orig_full[1] = full_mika01;
+				orig_full[2] = full_rinne01;
+				orig_full[3] = full_asch01;
+				orig_full[4] = full_noi01;
+				orig_full[5] = full_bubbles;
+				orig_full[6] = full_narancia01;
+				orig_full[7] = full_bede01;
+				orig_full[8] = full_mother3_photography;
+				orig_full[9] = full_projeto01;
+				orig_full[10] = full_giorno01;
+				orig_full[11] = full_holymother;
+
+		// descrições
+
+			// original
+
+			//	var orig_desc = [];
+
+			//	orig_desc[0] = desc_bubbles;
+			//	orig_desc[1] = desc_projcadernos;
+			//	orig_desc[2] = desc_holymother;
+			//	orig_desc[3] = desc_notanybetter;
---
+++ js/translate.js
@@ -0,0 +1,102 @@
+// Starts in english
+var lang = 0;
+
+window.onload = function() {
+  alert(lang);
+//	currentPage('index', lang);
+  translate();
+}
+
+function currentPage(i, language) {
+	if (i == 'index') {
+		document.getElementById("about_container").style.display = "none";
+		document.getElementById("illustration_container").style.display = "none";
+		document.getElementById("design_container").style.display = "none";
+		document.getElementById("index_container").style.display = "flex";
+
+		document.body.style.overflowY = "hidden";
+	}
+
+	if (i == 'about') {
+		document.getElementById("index_container").style.display = "none";
+		document.getElementById("illustration_container").style.display = "none";
+		document.getElementById("design_container").style.display = "none";
+		document.getElementById("about_container").style.display = "flex";
+
+		document.body.style.overflowY = "hidden";
+	}
+
+	if (i == 'illustration') {
+		document.getElementById("about_container").style.display = "none";
+		document.getElementById("index_container").style.display = "none";
+		document.getElementById("design_container").style.display = "none";
+		document.getElementById("illustration_container").style.display = "inline";
+
+		illustTab('original')
+		thumbnails_illust();
+		document.getElementById("button_orig").disabled = true;
+		document.getElementById("button_fan").disabled = false;
+
+		document.body.style.overflowY = "scroll";
+	}
+
+	if (i == 'design') {
+		document.getElementById("about_container").style.display = "none";
+		document.getElementById("index_container").style.display = "none";
+		document.getElementById("illustration_container").style.display = "none";
+		document.getElementById("design_container").style.display = "inline";
+
+		thumbnails_illust();
+
+		document.body.style.overflowY = "scroll";
+	}
+
+
+
+	if (language != 0 && language != 1) {
+		translate(language);
+		document.getElementById("eng").disabled = true;
+		document.getElementById("ptbr").disabled = false;
+	}
+}
+
+function changeLanguage() {
+
+  if (lang != 0 && lang != 1)
+    lang = 0;
+  else
+    if (lang == 0){
+      lang = 1;
+    }
+    else
+      lang = 0;
+  translate();
+}
+
+function translate() {
+
+	// TRANSLATE TO ENG
+	if (lang == 0)	{
+    // NAVIGATION
+		document.getElementById("navi_lang").innerHTML = navi[0];
+		document.getElementById("navi_about").innerHTML = navi[2];
+		document.getElementById("navi_experience").innerHTML = navi[4];
+    document.getElementById("navi_education").innerHTML = navi[6];
+    document.getElementById("navi_projects").innerHTML = navi[8];
+    document.getElementById("navi_skills").innerHTML = navi[10];
+    document.getElementById("navi_contact").innerHTML = navi[12];
+    // NAME & OCCUPATION
+    document.getElementById("myname").innerHTML = myname[0];
+	}
+  
+  // TRANSLATE TO PTBR
+	if (lang == 1)	{
+		document.getElementById("navi_lang").innerHTML = navi[1];
+		document.getElementById("navi_about").innerHTML = navi[3];
+		document.getElementById("navi_experience").innerHTML = navi[5];
+    document.getElementById("navi_education").innerHTML = navi[7];
+    document.getElementById("navi_projects").innerHTML = navi[9];
+    document.getElementById("navi_skills").innerHTML = navi[11];
+    document.getElementById("navi_contact").innerHTML = navi[13];
+	}
+}
