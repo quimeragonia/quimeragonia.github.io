6052ff9929f308fb2c1ce413435f9849240e442d
quimeranoxia <dreiruivo@gmail.com>
2021-12-24 19:23:29 -0300

    uma putaria de coisa

--- js/worksscripts.js
+++ js/worksscripts.js
@@ -5,6 +5,8 @@ function loadWorks () {
       document.getElementById("img" + String(n)).src = thumbnail[n];
       document.getElementById("full" + String(n)).src = full[n];
   }
+    for (n = 0; n < 1 ;n++)
+      document.getElementById("project" + String(n)).src = thumbproject[n];
 }
 
 function ampliar (i) {
