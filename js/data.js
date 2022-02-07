d54706cedb0d48e2a6b98a88f4a586b95afb6562
quimeranoxia <dreiruivo@gmail.com>
2022-02-07 18:39:24 -0300

    testecommit

--- js/data.js
+++ js/data.js
@@ -9,7 +9,7 @@
 						// Language
 
 							// when it's in english
-								navi[0] = "Em Português asdadasdadadsa";
+								navi[0] = "Em Português testeee";
 							// when it's in portuguese
 								navi[1] = "In English";
 
