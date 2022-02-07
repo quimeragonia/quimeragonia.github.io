d28ef0ec9aa14a7a571b59b038ba3e997c6aef36
quimeranoxia <dreiruivo@gmail.com>
2022-02-07 19:58:23 -0300

    TESTEPFVR

--- js/data.js
+++ js/data.js
@@ -9,7 +9,7 @@
 						// Language
 
 							// when it's in english
-								navi[0] = "Em Português testeee";
+								navi[0] = "Em Português testeeee5";
 							// when it's in portuguese
 								navi[1] = "In English";
 
